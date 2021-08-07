0a38cafa9466d56a7595f18211c8e58de5bed474
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-08-07 10:17:34 +0200

    Add car model entity management

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\CarMakeController;
+use App\Http\Controllers\Admin\CarModelController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,6 +17,15 @@ Route::group(['prefix' => "admin"], function (){
             Route::get('/edit/{id}',[CarMakeController::class, 'edit'])->name('car-make.edit');
             Route::put('/update/{id}',[CarMakeController::class, 'update'])->name('car-make.update');
             Route::put('/toggle-activation',[CarMakeController::class, 'toggleActivation'])->name('car-make.toggle-activation');
+        });
+
+        Route::group(['prefix' => 'car-model'],function () {
+            Route::get('/',[CarModelController::class, 'index'])->name('car-model.index');
+            Route::get('/create',[CarModelController::class, 'create'])->name('car-model.create');
+            Route::post('/',[CarModelController::class, 'store'])->name('car-model.store');
+            Route::get('/edit/{id}',[CarModelController::class, 'edit'])->name('car-model.edit');
+            Route::put('/update/{id}',[CarModelController::class, 'update'])->name('car-model.update');
+            Route::delete('/delete/{id}',[CarModelController::class, 'destroy'])->name('car-model.delete');
         });
     });
 });
