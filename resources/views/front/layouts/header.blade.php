0178700ec9dd0c3b9c300e797fba8bc0b2f2a6e6
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-08-14 16:03:19 +0200

    Remove text under logo in header

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg">
         <div class="container">
             <a class="navbar-brand" href="javascript:void(0)">
-                <img src="{{asset('images/logoWText-min.png')}}" alt="..."> <h2>Dourdo <em>Cars</em></h2>
+                <img src="{{asset('images/logoWText-min.png')}}" alt="...">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
