e3dd519279f115cd2f41aa341151d23ea0c6e08d
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-08-16 10:05:33 +0200

    Add loading for related car model years

--- app/Http/Controllers/Front/CarModelController.php
+++ app/Http/Controllers/Front/CarModelController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Http\Resources\YearResource;
 use App\Models\CarModel;
 use App\Services\CarModelService;
+use App\Services\CarService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -16,11 +18,12 @@ use Illuminate\Http\Response;
 class CarModelController extends Controller
 {
 
-    private $carModelService;
+    private $carModelService, $carService;
 
-    public function __construct(CarModelService $carModelService)
+    public function __construct(CarModelService $carModelService, CarService $carService)
     {
         $this->carModelService = $carModelService;
+        $this->carService = $carService;
     }
 
 
@@ -28,5 +31,11 @@ class CarModelController extends Controller
     {
         $carMakeModels = $this->carModelService->getModelsByCarMake($carMakeId);
         return \response()->json(['data' => $carMakeModels]);
+    }
+
+    public function getCarModelYears($carModelId)
+    {
+        $carModelYears = $this->carService->getCarModelYears($carModelId);
+        return \response()->json(['data' => YearResource::collection($carModelYears)]);
     }
 }
