a2bda3e85ada64c857b8b9566adec2bdc0b63cbe
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-09-04 11:39:00 +0200

    Add style and script version for force update

--- resources/views/front/layouts/foot.blade.php
+++ resources/views/front/layouts/foot.blade.php
@@ -6,7 +6,7 @@
 <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=61333a08495f05001236fe42&product=inline-share-buttons" async="async"></script>
 
 <!-- Additional Scripts -->
-<script src="{{asset('js/custom.js')}}"></script>
+<script src="{{asset('js/custom.js?v=1.0')}}"></script>
 <script src="{{asset('js/owl.js')}}"></script>
 @yield('js')
 </body>
