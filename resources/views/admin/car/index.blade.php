19e8e7e4f4407a81c614d8fa91b554ec043ace5b
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-09-05 11:28:38 +0200

    Add car filter with kilometers, make filteration fields optional

--- resources/views/admin/car/index.blade.php
+++ resources/views/admin/car/index.blade.php
@@ -91,7 +91,10 @@
 @endsection
 
 @section('js')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous"></script>
     <script src="{{asset('vendors/simple-datatables/simple-datatables.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
