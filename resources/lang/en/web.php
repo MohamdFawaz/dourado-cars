ea69909444e80c0bd97e9bb35dad09cdd2073fac
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-09-08 09:34:22 +0200

    Add locles from server

--- resources/lang/en/web.php
+++ resources/lang/en/web.php
@@ -1,58 +1,176 @@
 <?php
 
 return array (
-  'cylinders' => 'Cylinders',
-  'currency_name' => 'AED',
-  'footer_about_us' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra, nibh in fringilla molestie, turpis massa bibendum sem, et suscipit diam orci non turpis.',
-  'footer_contact_us_first_number' => '000000000000',
-  'footer_contact_us_second_number' => '0000000000000',
-  'footer_contact_us_third_number' => '000000000000000',
-  'footer_sell_a_car_first_number' => '5555555555555',
-  'footer_sell_a_car_second_number' => '666666666666',
-  'footer_sell_a_car_third_number' => '7777777777777',
-  'footer' => 
-  array (
-    'get_a_quote' => 'Get A',
-    'get_a' => 'Get A',
-    'quote' => 'quote',
-    'links_header' => 'Links',
-    'contact_us_header' => 'Contact Us',
-    'sell_a_car_header' => 'Sell Your Car',
-  ),
-  'sell_a_car' => 
-  array (
-    'car_make_label' => 'Car Make Label',
-    'car_information_title' => 'Car Information',
-  ),
-  'home' => 
-  array (
-    'filter' => 
-    array (
-      'car_make' => 
-      array (
-        'label' => 'Select Make',
-        'select' => 'Choose',
-      ),
-      'car_model' => 
-      array (
-        'label' => 'Select Model',
-        'select' => 'Choose',
-      ),
-      'year' => 
-      array (
-        'label' => 'Select Year',
-        'select' => 'Choose',
-      ),
-      'search_button' => 'GO',
-      'year_placeholder' => 'Choose',
-    ),
-  ),
-  'menus' => 
-  array (
-    'home' => 'Home',
-    'cars' => 'Cars',
-    'about_us' => 'About Us',
-    'sell_a_car' => 'Sell a Car',
-    'contact_us' => 'Contact Us',
-  ),
+    'footer_about_us' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra, nibh in fringilla molestie, turpis massa bibendum sem, et suscipit diam orci non turpis.',
+    'footer_contact_us_first_number' => '000000000000',
+    'footer_contact_us_second_number' => '0000000000000',
+    'footer_contact_us_third_number' => '000000000000000',
+    'footer_sell_a_car_first_number' => '5555555555555',
+    'footer_sell_a_car_second_number' => '666666666666',
+    'footer_sell_a_car_third_number' => '7777777777777',
+    'footer' =>
+        array (
+            'get_a' => 'Get A',
+            'quote' => 'Quote',
+            'links_header' => 'Links',
+            'contact_us_header' => 'Contact Us',
+            'sell_a_car_header' => 'Sell Your Car',
+            'visit_our_showroom' =>
+                array (
+                    'title' => 'Our Latest Videos',
+                    'get_in_touch_title' => 'Get In Touch',
+                    'form' =>
+                        array (
+                            'full_name_placeholder' => 'Full Name',
+                            'mobile_number_placeholder' => 'Mobile No.',
+                            'preferred_date_placeholder' => 'Preferred Date',
+                            'preferred_time_placeholder' => 'Submit Request',
+                        ),
+                ),
+            'visit_showroom' =>
+                array (
+                    'success_message' => 'Success',
+                    'error_message' => 'Error',
+                ),
+        ),
+    'menus' =>
+        array (
+            'home' => 'Home',
+            'cars' => 'Cars',
+            'about_us' => 'About Us',
+            'sell_a_car' => 'Sell a Car',
+            'contact_us' => 'Contact Us',
+        ),
+    'home' =>
+        array (
+            'filter' =>
+                array (
+                    'car_make' =>
+                        array (
+                            'select' => 'Choose',
+                            'label' => 'Select Make',
+                        ),
+                    'car_model' =>
+                        array (
+                            'select' => 'Choose',
+                            'label' => 'Select Model',
+                        ),
+                    'year' =>
+                        array (
+                            'label' => 'Select Year',
+                            'select' => 'Choose',
+                        ),
+                    'year_placeholder' => 'Choose',
+                    'search_button' => 'GO',
+                ),
+            'most_popular_title' => 'Most Popular',
+            'view_more_title' => 'View More',
+            'latest_car_make' =>
+                array (
+                    'title' => 'Car Make',
+                    'see_all' => 'View All',
+                ),
+            'contact' =>
+                array (
+                    'title' => 'Contact Us',
+                    'content' => 'Contact Us Content',
+                    'button_text' => 'Contact Us',
+                ),
+            'advanced_filter' =>
+                array (
+                    'car_make' =>
+                        array (
+                            'label' => 'Car Make',
+                            'select' => 'Select',
+                        ),
+                    'car_model' =>
+                        array (
+                            'label' => 'Model',
+                            'select' => 'Select',
+                        ),
+                    'year' =>
+                        array (
+                            'label' => 'Year',
+                            'select' => 'Select',
+                        ),
+                    'mileage' =>
+                        array (
+                            'label' => 'K.m',
+                            'placeholder' => 'Enter',
+                        ),
+                    'price' => 'Price Range',
+                    'search_button' => 'Search',
+                ),
+            'about_us_title' => 'About Us',
+            'about_us_content' => 'About us Content',
+            'about_us_see_more' => 'More Info',
+        ),
+    'currency_name' => 'AED',
+    'cylinders' => 'Cylinders',
+    'header' =>
+        array (
+            'social_media' =>
+                array (
+                    'facebook' => 'Facebook',
+                    'instagram' => 'Instagram',
+                    'twitter' => 'Twitter',
+                    'youtube' => 'Youtube',
+                    'linkedin' => 'LinkedIn',
+                ),
+            'contact_number' => '00971566655888',
+        ),
+    'title' =>
+        array (
+            'home' => 'Home',
+            'site_name' => 'DouradoCars',
+            'cars' => 'Cars',
+            'about_us' => 'About Us',
+        ),
+    'car_details' =>
+        array (
+            'car_make' => 'Car Make',
+            'car_model' => 'Model',
+            'mileage' => 'K.m',
+            'year' => 'Year',
+            'price' => 'Price',
+            'has_warranty_label' => 'Warranty',
+            'has_warranty' =>
+                array (
+                    'yes' => 'No',
+                    'no' => 'Yes',
+                ),
+            'color' => 'Color',
+            'number_of_doors' => 'Doors',
+            'number_of_cylinders' => 'Cylinders',
+            'horse_power' => 'Horse Power',
+            'specs' => 'Specifications',
+            'transmission_type' => 'Transmission',
+            'body_type' => 'Body Type',
+            'fuel_type' => 'Fuel',
+            'compare' => 'Compare',
+            'additional_information' => 'Additional Info',
+        ),
+    'pages' =>
+        array (
+            'list_cars' =>
+                array (
+                    'upper_title' => 'Luxurious Cars',
+                    'lower_title' => 'Explore Our Collection',
+                ),
+            'about' =>
+                array (
+                    'upper_title' => 'Dourado Luxury Cars',
+                    'lower_title' => 'About Us',
+                ),
+        ),
+    'page' =>
+        array (
+            'about' =>
+                array (
+                    'section_1_heading' => 'About The Company',
+                    'section_1_content' => 'Description 1',
+                    'section_2_heading' => 'Vision',
+                    'section_2_content' => 'Description 2',
+                ),
+        ),
 );
