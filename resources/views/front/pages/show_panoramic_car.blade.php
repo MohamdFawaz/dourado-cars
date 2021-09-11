5a0d1d9d0613c593cfd918daf78781f8f3376259
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-09-11 14:52:43 +0200

    Add fix to panoramic view

--- resources/views/front/pages/show_panoramic_car.blade.php
+++ resources/views/front/pages/show_panoramic_car.blade.php
@@ -1,7 +1,9 @@
+<head>
+    <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+</head>
 <a-scene>
     <a-assets>
         <img id="panorama" src="{{asset('images/360 picture 1.jpg')}}" crossorigin />
     </a-assets>
     <a-sky src="#panorama" rotation="0 -90 0"></a-sky>
 </a-scene>
-<script src="https://aframe.io/releases/0.6.1/aframe.min.js"></script>
