d4adf1529311787ca732fee492a85eb1c2221daa
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-09-18 15:57:46 +0200

    Fix compare resposive on width of 360

--- resources/views/front/layouts/head.blade.php
+++ resources/views/front/layouts/head.blade.php
@@ -26,7 +26,7 @@
     @if(app()->getLocale() == 'ar')
         <link rel="stylesheet" href="{{asset('css/style.rtl.css?v=1.2')}}">
     @else
-        <link rel="stylesheet" href="{{asset('css/style.min.css?v=1.24')}}">
+        <link rel="stylesheet" href="{{asset('css/style.min.css?v=1.25')}}">
     @endif
 
     <link rel="stylesheet" href="{{asset('css/owl.css')}}">
