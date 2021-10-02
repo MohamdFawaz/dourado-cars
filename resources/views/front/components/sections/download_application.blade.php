7d00075801ff900be8091e7534ad2b933161d18a
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-10-02 16:29:57 +0200

    Add download app section

---
+++ resources/views/front/components/sections/download_application.blade.php
@@ -0,0 +1,28 @@
+<div class="download-application">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center pt-3">
+
+                <h3>{{trans('web.page.home.download_application_title')}}</h3>
+                <ul>
+                    <li>
+                        <a href="https://play.google.com/store/apps/details?id=com.douradocars" target="_blank">
+                            <div class="download android">
+                                <i class="fa fa fa-android fa-3x"></i>
+                                <span class="dfn">Google Play</span>
+                            </div>
+                        </a>
+                    </lI>
+                    <li>
+                        <a href="https://apps.apple.com/ca/app/dourado-cars/id1586962753?ign-mpt=uo%3D2" target="_blank">
+                            <div class="download apple">
+                                <i class="fa fa fa-apple fa-3x"></i>
+                                <span class="dfn">App Store</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
